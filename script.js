269fce993f0b934349a370e77dac357e3300dc8b
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-29 19:13:27 -0800

    add functionality to calculate all user's wealth

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ getRandomUser();
 getRandomUser();
 
 // Functions
+
+// Fetch Random User from API and add money
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
-  
   const user = data.results[0];
-
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   };
-
   addData(newUser);
 }
 
+// Double every user's money
 function doubleMoney() {
   data = data.map(user => {
     return { ...user, money: user.money * 2 };
@@ -36,24 +36,37 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort user's by wealth
 function sortByRichest() {
   data.sort((a, b) => b.money - a.money);
 
   updateDOM();
 }
 
+// Filter only millionaires
 function showMillionaires() {
   data = data.filter(user => user.money > 1000000);
 
   updateDOM();
 }
 
+// Calculate total wealth of users
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthElement = document.createElement('div');
+  wealthElement.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</h3>`;
+  main.appendChild(wealthElement);
+}
+
+// Add new object to data array
 function addData(obj) {
   data.push(obj);
 
   updateDOM();
 }
 
+// Update DOM with new data
 function updateDOM(providedData = data) {
   main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
   
@@ -61,11 +74,12 @@ function updateDOM(providedData = data) {
   providedData.forEach(item => {
     const element = document.createElement('div');
     element.classList.add('person');
-    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
     main.appendChild(element);
   });
 }
 
+// Format numbers into readable money format
 function formatMoney(number) {
   return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
@@ -75,3 +89,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionBtn.addEventListener('click', showMillionaires);
+calculateBtn.addEventListener('click', calculateWealth);
