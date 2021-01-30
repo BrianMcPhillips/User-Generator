ddc1ca963c972dc53e1f88afe44f27ff17eab2cb
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-29 17:26:39 -0800

    complete add user button

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ getRandomUser();
 
 // Functions
 async function getRandomUser() {
-  const res = await fetch('https//randomuser.me/api');
+  const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
   
   const user = data.results[0];
@@ -30,4 +30,25 @@ async function getRandomUser() {
 
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+function updateDOM(providedData = data) {
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+  
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    main.appendChild(element);
+  });
+}
+
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listener 
+addUserBtn.addEventListener('click', getRandomUser);
